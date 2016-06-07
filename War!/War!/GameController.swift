75e265e4d73364b8a525394eb4071321a6f81797
Steve Cox <coxsteve@gmail.com>
2016-06-07 10:09:48 -0600

    adding our blank controller files

---
+++ War!/War!/GameController.swift
@@ -0,0 +1,9 @@
+//
+//  GameController.swift
+//  War!
+//
+//  Created by Steve Cox on 6/7/16.
+//  Copyright © 2016 SteveCox. All rights reserved.
+//
+
+import Foundation
---
+++ War!/War!/NetworkController.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkController.swift
+//  War!
+//
+//  Created by Steve Cox on 6/7/16.
+//  Copyright © 2016 SteveCox. All rights reserved.
+//
+
+import Foundation
