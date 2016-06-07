e919cc81dd5080c80fe4b4ed8356fa6ffe3c7759
Steve Cox <coxsteve@gmail.com>
2016-06-07 09:25:07 -0600

    notes

---
+++ Steetcheerrr.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+
+/*
+ 
+ global queues already created by iOS:
+ 
+ QOS_CLASS_USER_INTERACTIVE
+ 
+ QOS_CLASS_USER_INITIATED
+ 
+ QOS_CLASS_UTILITY
+ 
+ QOS_CLASS_BACKGROUND
+ 
+ 
+ */
