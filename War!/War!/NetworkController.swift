dbade6805cd62e7ce0652dac08ce382f7d9dc954
Steve Cox <coxsteve@gmail.com>
2016-06-07 10:39:55 -0600

    going through the network controller more slowly

--- War!/War!/NetworkController.swift
+++ War!/War!/NetworkController.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+class NetworkController {
+
+    static func dataAtURL(url: String, completion: (data: NSData?) -> Void) {
+    
+        // STEP 1 = create URL
+        guard let url = NSURL(string: url) else {completion (data: nil); return }
+        
+        // STEP 2 = create our task from the shared instance of NSURLSession
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, _, error) in
+        
+        // STEP 3 = check if there was an error or not
+            if let error = error {
+                //print our error description
+                print(error.localizedDescription)
+                completion(data: nil)
+                return
+            }
+        // STEP 4 = check if we have data or not
+            if let data = data {
+                // print out that we have data and complete with that data
+                print("We have data!")
+                completion(data: data)
+            }
+        }
+        //STEP 5 = start the task
+    task.resume()
+    
+    }
+
+}
